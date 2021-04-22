41ca61fbe977ba14346179a60d2255068fa49bb0
Kuba2cg22 <81517493+Kuba2cg22@users.noreply.github.com>
2021-04-22 14:31:54 +0200

    Add files via upload

---
+++ zagadki.js
@@ -0,0 +1,55 @@
+function z1()
+{
+     document.getElementById("z1").innerHTML="Tylko jeden, ten zabity. Pozostałe oczywiście uciekły.";
+}
+
+function z2()
+    {
+        document.getElementById("z1").innerHTML="Tu znajdziesz odpowiedź";
+    } 
+
+
+
+function z3()
+{
+     document.getElementById("z3").innerHTML="Miłośnik rozrywek miał 3 siostry i 3 braci.";
+}
+    
+function z4()
+    {
+        document.getElementById("z3").innerHTML="Tu znajdziesz odpowiedź";
+    }   
+        
+
+
+function z5()
+{
+     document.getElementById("z5").innerHTML="Było 6, niosło 3 i dwóch kolejnych niosło kajak i wiosło";
+}
+        
+function z6()
+    {
+        document.getElementById("z5").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+function z7()
+{
+     document.getElementById("z7").innerHTML="4 kwiaty. Dwie stokrotki oraz po jednym słoneczniku i hiacyncie";
+}
+
+function z8()
+    {
+        document.getElementById("z7").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+    function z9()
+{
+     document.getElementById("z9").innerHTML="Siedem (suma liczby samogłosek).";
+}
+
+function z10()
+    {
+        document.getElementById("z9").innerHTML="Tu znajdziesz odpowiedź";
+    }
